f9861ea4df0d6e43a38b4704ab75c3369f4283ea
Ayman Issa <ayman.issa92@gmail.com>
2020-11-13 20:22:12 +0300

    Created activity_movie_details.xml

--- app/src/main/java/com/aimanissa/android/androidacademyproject/MovieDetailsActivity.kt
+++ app/src/main/java/com/aimanissa/android/androidacademyproject/MovieDetailsActivity.kt
@@ -2,13 +2,10 @@ package com.aimanissa.android.androidacademyproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
-
     }
-
 }
